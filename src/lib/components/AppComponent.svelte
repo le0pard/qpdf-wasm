3ea5b047903f016e17eaea1620b35c1db12c6c26
Alexey Vasiliev <leopard.not.a@gmail.com>
2026-01-04 18:21:50 +0200

    add splitter

--- src/lib/components/AppComponent.svelte
+++ src/lib/components/AppComponent.svelte
@@ -1,13 +1,58 @@
 <script>
+  import SplitViewComponent from '$lib/components/SplitViewComponent.svelte'
+
+  import { onMount, onDestroy, setContext } from 'svelte'
+  import { splitState } from '$lib/stores/split'
+
   let { webWorkerObject } = $props()
 
-  import { onMount } from 'svelte'
+  setContext('ww', {
+    getWebWorker: () => webWorkerObject
+  })
 
-  onMount(() => {
-    webWorkerObject.pdfInfo().then(() => {
-      // console.log('res', res)
-    })
+  onDestroy(() => {
+    splitState.reset()
   })
 </script>
 
-<h1>App</h1>
+<div class="app-view">
+  <div class="app-controls" class:app-controls-hidden={$splitState.visible === 'right'}>
+    <h1>Test</h1>
+  </div>
+  <SplitViewComponent />
+  <div class="app-pdf-view" class:app-pdf-view-hidden={$splitState.visible === 'left'}>
+    <h1>Test</h1>
+  </div>
+</div>
+
+<style>
+  .app-view {
+    display: flex;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+  }
+
+  .app-controls {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    flex: 1;
+    position: relative;
+  }
+
+  .app-pdf-view {
+    height: 100%;
+    flex: 1;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .app-controls-hidden,
+  .app-pdf-view-hidden {
+    display: none;
+  }
+</style>
---
+++ src/lib/components/SplitViewComponent.svelte
@@ -0,0 +1,131 @@
+<script>
+  import { onMount } from 'svelte'
+  import { splitState, screenSizeMinMedia } from '$lib/stores/split'
+
+  const onScreenSizeMinMediaChange = (e) => {
+    if (e.matches) {
+      splitState.hideForceRight()
+    }
+  }
+
+  const handleHideLeftKey = (e) => {
+    e.preventDefault()
+    if (e.key === ' ' || e.code === 'Space' || e.keyCode === 32) {
+      splitState.hideLeft()
+    }
+  }
+
+  const handleHideLeft = (e) => {
+    e.preventDefault()
+    splitState.hideLeft()
+  }
+
+  const handleHideRightKey = (e) => {
+    e.preventDefault()
+    if (e.key === ' ' || e.code === 'Space' || e.keyCode === 32) {
+      splitState.hideRight()
+    }
+  }
+
+  const handleHideRight = (e) => {
+    e.preventDefault()
+    splitState.hideRight()
+  }
+
+  onMount(() => {
+    const eventAbortController = new AbortController()
+    const { signal } = eventAbortController
+    // init
+    onScreenSizeMinMediaChange(screenSizeMinMedia())
+    // subscribe
+    screenSizeMinMedia().addEventListener('change', onScreenSizeMinMediaChange, { signal })
+    return () => eventAbortController?.abort()
+  })
+</script>
+
+<div class="split-container">
+  <div
+    onclick={handleHideLeft}
+    onkeypress={handleHideLeftKey}
+    tabindex="0"
+    role="button"
+    class="split-left"
+    class:split-hidden={$splitState.visible === 'right'}
+  >
+    <i class="arrow-left"></i>
+  </div>
+  <div
+    onclick={handleHideRight}
+    onkeypress={handleHideRightKey}
+    tabindex="0"
+    role="button"
+    class="split-right"
+    class:split-hidden={$splitState.visible === 'left'}
+  >
+    <i class="arrow-right"></i>
+  </div>
+</div>
+
+<style>
+  .split-container {
+    display: flex;
+    width: 30px;
+  }
+
+  .split-left,
+  .split-right {
+    align-items: center;
+    background-color: var(--splitBgColor);
+    color: var(--splitColor);
+    cursor: pointer;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  .split-left {
+    border-left: 1px solid var(--splitBorderColor);
+    border-right: 1px solid var(--splitBorderColor);
+  }
+
+  .split-right {
+    border-right: 1px solid var(--splitBorderColor);
+  }
+
+  .split-left:hover,
+  .split-right:hover {
+    background-color: var(--splitBgHoverColor);
+  }
+
+  .split-left:active,
+  .split-right:active {
+    background-color: var(--splitBgHoverColor);
+  }
+
+  .arrow-left,
+  .arrow-right {
+    border-style: solid;
+    border-color: var(--splitColor);
+    border-width: 0 2px 2px 0;
+    display: inline-block;
+    padding: 2px;
+  }
+
+  .split-left:hover .arrow-left,
+  .split-right:hover .arrow-right {
+    border-color: var(--splitHoverColor);
+  }
+
+  .arrow-left {
+    transform: rotate(135deg);
+  }
+
+  .arrow-right {
+    transform: rotate(-45deg);
+  }
+
+  .split-hidden {
+    display: none;
+  }
+</style>
